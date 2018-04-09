b2568116d7a43ead193ffbee733b5c26fc807eef
cfgs <cfgs@pc-info-host>
2018-04-09 16:10:33 +0200

    cambios de marcha

--- src/vehicles/Vehicle.java
+++ src/vehicles/Vehicle.java
@@ -5,10 +5,21 @@ public class Vehicle {
 	private int portes;
 	private int velocitat_actual;
 	private static int velocitat_maxima = 120;
+	private static int marxes = 5;
+	private int marxaActual;
+
 
 	public Vehicle() {
 		super();
 		this.velocitat_actual = 0;
+		this.marxaActual = 0;
+	}
+
+	public Vehicle(int cilindrada, int portes, int velocitat_actual) {
+		super();
+		this.cilindrada = cilindrada;
+		this.portes = portes;
+		this.velocitat_actual = velocitat_actual;
 	}
 
 	public void Accelerar(){
@@ -21,4 +32,15 @@ public class Vehicle {
 			velocitat_actual -= 10;
 		}
 	}
+	public void pujaMarxa(){
+		if(marxaActual < marxes){
+			marxaActual ++;
+		}
+	}
+	public void baixaMarxa(){
+		if(marxaActual > 0){
+			marxaActual --;
+		}
+	}
+
 }
